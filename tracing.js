b4a0cd2394e73c4c2c7077ad5300dd51d62e0355
steviegalluscio <galluscio.stevie@gmail.com>
2023-04-18 19:55:37 -0400

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -1,6 +1,5 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-//const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
